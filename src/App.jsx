8d27f81231dc7fd3b07caebbab939917c7323d2a
Israel Duran <israel_duran@live.com>
2023-05-22 13:21:07 -0600

    dia 3 update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import TodoComputed from "./components/TodoComputed";
 import TodoCreate from "./components/TodoCreate";
 import TodoFilter from "./components/TodoFilter";
 import TodoList from "./components/TodoList";
 
-const initialStateTodo = [
-  { id: 1, title: "gotothegym", completed: true },
-  { id: 2, title: "complete online js cuorse", completed: false },
-  { id: 3, title: "10 minutos meditar", completed: false },
-  { id: 4, title: "escoger algo", completed: false },
-  { id: 5, title: "complete reto", completed: true },
-];
+// const initialStateTodo = [
+//   { id: 1, title: "gotothegym", completed: true },
+//   { id: 2, title: "complete online js cuorse", completed: false },
+//   { id: 3, title: "10 minutos meditar", completed: false },
+//   { id: 4, title: "escoger algo", completed: false },
+//   { id: 5, title: "complete reto", completed: true },
+// ];
+const initialStateTodo = JSON.parse(localStorage.getItem("todos")) || [];
 
 const App = () => {
   const [todos, setTodos] = useState(initialStateTodo);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  },[todos])
 
   const createTodo = (title) => {
     const newTodo = {
@@ -60,10 +65,12 @@ const App = () => {
     <div
       className="min-h-screen bg-gray-300 
         bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat 
-        dark:bg-gray-900 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')]"
+        dark:bg-gray-900 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] 
+        md:bg-[url('./assets/images/bg-desktop-light.jpg')] 
+        md:dark:bg-[url('./assets/images/bg-desktop-dark.jpg')]"
     >
       <Header />
-      <main className="container mx-auto mt-8 px-4">
+      <main className="container mx-auto mt-8 px-4 md:max-w-xl">
         <TodoCreate createTodo={createTodo} />
 
         <TodoList
