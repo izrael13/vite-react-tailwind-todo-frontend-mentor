8901a9a9a49d52119828ec613bd455f5ba0eca57
Israel Duran <israel_duran@live.com>
2023-05-23 16:22:22 -0600

    dia 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import {DragDropContext} from "@hello-pangea/dnd"
 import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import TodoComputed from "./components/TodoComputed";
@@ -13,6 +14,14 @@ import TodoList from "./components/TodoList";
 //   { id: 5, title: "complete reto", completed: true },
 // ];
 const initialStateTodo = JSON.parse(localStorage.getItem("todos")) || [];
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = [...list];
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
 
 const App = () => {
   const [todos, setTodos] = useState(initialStateTodo);
@@ -61,6 +70,20 @@ const App = () => {
     }
   }
 
+  const handleDragEnd = (result) => {
+    const { destination, source } = result;
+    if (!destination) return;
+    if (
+        source.index === destination.index &&
+        source.droppableId === destination.droppableId
+    )
+        return;
+
+    setTodos((prevTasks) =>
+        reorder(prevTasks, source.index, destination.index)
+    );
+  };
+
   return (
     <div
       className="min-h-screen bg-gray-300 
@@ -73,11 +96,13 @@ const App = () => {
       <main className="container mx-auto mt-8 px-4 md:max-w-xl">
         <TodoCreate createTodo={createTodo} />
 
-        <TodoList
-          todos={filteredTodos()}
-          updateTodo={updateTodo}
-          removeTodo={removeTodo}
-        />
+        <DragDropContext onDragEnd={handleDragEnd}>
+          <TodoList
+            todos={filteredTodos()}
+            updateTodo={updateTodo}
+            removeTodo={removeTodo}
+          />
+        </DragDropContext>
 
         <TodoComputed computedItemsLeft={computedItemsLeft} clearCompleted={clearCompleted}/>
 
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,17 +1,36 @@
+import {Droppable, Draggable} from "@hello-pangea/dnd"
 import TodoItem from "./TodoItem";
 
 const TodoList = ({ todos, updateTodo, removeTodo }) => {
   return (
-    <div className="mt-8 overflow-hidden rounded-t-md bg-white [&>article]:p-4">
-      {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          updateTodo={updateTodo}
-          removeTodo={removeTodo}
-        />
-      ))}
-    </div>
+    <Droppable droppableId="todos">
+      {
+        (droppableProvider) => (
+          <div 
+            ref={droppableProvider.innerRef} 
+            {...droppableProvider.droppableProps}
+            className="mt-8 overflow-hidden rounded-t-md bg-white [&>article]:p-4">
+          {todos.map((todo, index) => (
+            <Draggable key={todo.id} index={index} draggableId={`${todo.id}`}>
+              {
+                (draggableProvider) => (
+                  <TodoItem
+                  todo={todo}
+                  updateTodo={updateTodo}
+                  removeTodo={removeTodo}
+                  ref={draggableProvider.innerRef}
+                  {...draggableProvider.dragHandleProps}
+                  {...draggableProvider.draggableProps}
+                />
+                )
+              }
+            </Draggable>
+          ))}
+          {droppableProvider.placeholder}
+        </div>
+        )
+      }
+    </Droppable>
   );
 };
 export default TodoList;
